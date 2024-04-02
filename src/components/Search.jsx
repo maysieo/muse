8b3a6bb15063f260ad943f7b6ea5a5f0a27e6299
maysieo <maysieocera@gmail.com>
2024-04-02 17:46:34 -0400

    Stubbing out searching for artists component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import axios from 'axios';
 
 function Search() {
+
+const [searchArtist, setSearchArtist] = useState('');
+const [artwork, setArtwork] = useState([]);
+
+function getSearchValue (e) {
+    setSearchArtist(e.target.value)
+  }
+
+function sendSearchValue () {
+  axios.get(`/?=${searchArtist}`)
+    .then((response) => {
+      setArtwork(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+}
+
   return (
     <div>
-      <input type="text" placeholder="Find artists to follow" />
+      <input type="text" placeholder="Find artists to follow" onChange={(e) => getSearchValue(e)} />
       <button>Search</button>
     </div>
   )
